6656b5bf6de8681cb01db0ad75a02450347bb76e
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-07-24 09:59:30 -0400

    commit tree 04284dd569ffd51f4669f4dff52f98c3216876dc

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,24 @@
 # your code here
+class Stack
+
+      def initialize
+        @stack = []
+      end
+
+      def push(value)
+        #push(value): add an element to the top of the stack
+        @stack.push(value)
+      end
+      
+      
+      def pop
+        @stack.pop
+        #pop: remove the element at the top of the stack
+       end
+
+       def peek
+        #peek: return the value of the element at the top of the stack without removing it
+        @stack.last
+       end
+
+end 
